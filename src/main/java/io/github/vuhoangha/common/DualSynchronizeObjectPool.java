595fbd329ae7527c0fa67774922afca67b01471c
Kendrick <vuhoangha100995@gmail.com>
2024-07-18 23:30:55 +0700

    pump 1.0.25

--- src/main/java/io/github/vuhoangha/common/DualSynchronizeObjectPool.java
+++ src/main/java/io/github/vuhoangha/common/DualSynchronizeObjectPool.java
@@ -3,29 +3,29 @@ package io.github.vuhoangha.common;
 import java.util.function.Supplier;
 
 /**
- * object pool kết hợp cả single thread và multithread
- * lấy object sẽ là single thread nên nhanh hơn, trả object sẽ multithread nên chậm hơn
- * khi việc lấy object mà object pool đồng bộ bị hết thì nó sẽ lấy từ object pool bất đồng bộ để đưa vào
- * việc đồng bộ giữa 2 object pool nội bộ sẽ khá tốn CPU nên class này nên sử dụng khi số lượng phần tử trong object pool lớn hơn nhiều so với số phần tử lấy ra
+ * object pool cho phép luồng lấy và nhận object ở 2 thread độc lập
+ * lấy và trả object đều là single thread nên nó nhanh tương đương nhau
+ * khi lấy object mà supply pool bị hết thì sẽ lấy từ consume pool đưa vào
+ * việc đồng bộ giữa 2 object pool nội bộ khá tốn CPU nên số lượng phần tử phải lớn 1 chút để hạn chế việc chuyển object giữa 2 pool
  */
-public class HybridObjectPool<T> {
+public class DualSynchronizeObjectPool<T> {
 
     private final SynchronizeObjectPool<T> supplyPool;      // pool để lấy object
-    private final ConcurrentObjectPool<T> consumePool;      // pool để trả object
-    private int capacity;
+    private final SynchronizeObjectPool<T> consumePool;      // pool để trả object
+    private final int capacity;
     private final Supplier<T> factory;
 
 
-    public HybridObjectPool(T[] pool, Supplier<T> factory) {
+    public DualSynchronizeObjectPool(T[] pool, Supplier<T> factory) {
         this.capacity = pool.length;
         this.factory = factory;
         supplyPool = new SynchronizeObjectPool<>(pool.clone(), factory);
-        consumePool = new ConcurrentObjectPool<>(pool.clone(), factory);
+        consumePool = new SynchronizeObjectPool<>(pool.clone(), factory);
     }
 
 
     public T pop() {
-        // nếu pool sync hết object thì lấy hết từ pool async chuyển qua
+        // nếu supply pool hết object thì lấy từ consume pool chuyển qua
         if (supplyPool.isEmpty()) {
             synchronized (consumePool) {
                 if (consumePool.isFull()) {      // phải chờ cho pool này full mới lấy tại vì tránh trường hợp tổng số lượng phần tử ít hơn kích thước pool nhưng cứ lấy qua lại sẽ ko hiệu quả. Ví dụ pool có capacity 1_000_000 nhưng tối đa chỉ có 100 phần tử thành ra cứ tới ngưỡng 100 nó đã chuyển rồi, ko hiệu quả

0775398d66eef68d170a5f2b894efe58b2d467ea
Kendrick <vuhoangha100995@gmail.com>
2024-11-15 10:39:44 +0700

    pump 1.0.43

---
+++ src/main/java/io/github/vuhoangha/common/RoundRobinObjectPool.java
@@ -0,0 +1,37 @@
+package io.github.vuhoangha.common;
+
+import java.util.Arrays;
+
+/**
+ * Pool chứa các object mà khi lấy ra sẽ trả lần lượt theo thuật toán Round Robin
+ * Không thread-safe
+ * Các phần tử phải tự thread-safe
+ * Không có hàm push, các phần tử phải khởi tạo hoàn toàn ngay từ đầu
+ */
+public class RoundRobinObjectPool<T> {
+
+    private T[] pool;
+    private long capacity;
+    private long sequence = -1;
+
+    public RoundRobinObjectPool(T[] pool) {
+        this.pool = pool;
+        this.capacity = pool.length;
+    }
+
+    public T get() {
+        int nextIndex = (int) (++sequence % capacity);
+        if (sequence == Long.MAX_VALUE) {
+            sequence = -1;
+        }
+        return pool[nextIndex];
+    }
+
+    public void clear() {
+        Arrays.fill(pool, null);
+        sequence = -1;
+        pool = null;
+        capacity = 0;
+    }
+
+}
